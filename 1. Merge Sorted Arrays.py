e0c8ce7f07438d92e87f45b5dce310b07e084fac
Swarup Panda <67518032+swaruppanda11@users.noreply.github.com>
2023-08-12 23:16:35 +0530

    Update 1. Merge Sorted Arrays.py

--- 1. Merge Sorted Arrays.py
+++ 1. Merge Sorted Arrays.py
@@ -1,8 +1,16 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-        return k
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+       last = m + n - 1
+
+       while m > 0 and n > 0:
+           if nums1[m - 1] > nums2[n - 1]:
+               nums1[last] = nums1[m - 1]
+               m -= 1
+           else:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+           last -= 1
+
+       while n > 0:
+            nums1[last] = nums2[n - 1]
+            n, last = n-1, last - 1
